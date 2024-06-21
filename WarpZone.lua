557b64a0b7ccfeb318019aad34c611c523b3b3c5
EnchantedAlchemy <154118441+EnchantedAlchemy@users.noreply.github.com>
2024-06-20 21:42:09 -0500

    Create WarpZone.lua

---
+++ WarpZone.lua
@@ -0,0 +1,30 @@
+local textFunctions = require("utilities/textFunctions")
+
+settings.load(".settings")
+if settings.get("warpName") == nil or settings.get("warpName") == "" or settings.get("warpName") == " " then
+
+	settings.define("warpName", {
+		description = "The name of the warp zone.",
+		default = "",
+		type = "string"
+	})
+
+	local newName = textFunctions.prompt("Enter a warp zone name:")
+	settings.set("warpName", newName)
+
+	settings.save(".settings")
+
+end
+
+print(settings.get("warpName"))
+peripheral.find("modem", rednet.open)
+rednet.host("warp_zone", settings.get("warpName"))
+
+while true do
+
+	local id, warpSide = rednet.recieve("warp_central")
+	redstone.setOutput(warpSide, true)
+	os.sleep()
+	redstone.setOutput(warpSide, false)
+
+end
